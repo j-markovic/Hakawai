c94035e9c06d95f596e6c5af098ad582cd81f75d
j-markovic <jovan.markovic@icraftapps.com>
2022-04-20 11:27:08 +0200

    SPM support

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Hakawai",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v10)],
+    products: [
+        .library(
+            name: "Hakawai",
+            targets: ["Hakawai"]),
+    ],
+    targets: [
+        .target(
+            name: "Hakawai",
+            dependencies: [],
+            path: "Hakawai",
+            publicHeadersPath: "",
+            cSettings: [
+                .headerSearchPath("Core"),
+                .headerSearchPath("Core/AbstractionLayer"),
+                .headerSearchPath("Core/TextKit"),
+                .headerSearchPath("Core/Default"),
+                .headerSearchPath("Mentions")
+            ]),
+    ]
+)
